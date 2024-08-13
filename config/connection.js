7327e707cd579dba35ffe34df497647b0f72c444
golusingh21 <golu.mandloi760@gmail.com>
2024-08-13 22:14:06 +0530

    change the db

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDb(){
     try{
         console.log('Connection established')
-        return await mongoose.connect(process.env.DB_URL);
+        return await mongoose.connect(`${process.env.DB_URL}/nodejs`);
     }catch(err){
         console.log('error', err)
     }
